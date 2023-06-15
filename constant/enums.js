56f5f2a4182f636fa4d390e4da94f0ec436e28c5
SivaprakashUnitedHands <sivaprakash.p@unitedhands.cc>
2023-06-15 16:40:17 +0530

    Initial commit

---
+++ constant/enums.js
@@ -0,0 +1,12 @@
+const REST_API_STATUSCODE = {
+    ok : 200,
+    created : 201,
+    noContent : 204,
+    badRequest: 400,
+    unauthorized : 401,
+    forbidden : 403,
+    notFound : 404,
+    internalServerError : 500
+}
+module.exports = {
+  REST_API_STATUSCODE}
